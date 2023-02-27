69cc07b9c42d4e9916ac4862b9418795f71e11d7
LeaMarz <marzonettoleandro@gmail.com>
2023-02-27 19:19:38 -0300

    Se Agrega mock de tareas e interface

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,15 @@
+export const TASK = [
+    {
+        id:1,
+        text: "trrminar primer modulo",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "trrminar 2dp modulo",
+        day: 'AGosto 10 a las 14:00',
+        reminder: true  
+    },
+
+]
