1d49c4c99abba31041d0e487c9ce992484bd4775
LeaMarz <marzonettoleandro@gmail.com>
2023-03-11 21:31:30 -0300

    ceracion de servicio de tareas

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input} from '@angular/core';
+import { Component , Input,OnInit, Output, EventEmitter} from '@angular/core';
 import { Task } from '../Task';
 import { TASK } from '../mock-task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -8,7 +8,11 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent {
-@Input() task: Task= TASK[0]
-faTimes=faTimes;
+  @Input() task: Task= TASK[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  faTimes=faTimes;
+  ngOnInit():void {} 
+  onDelete(task : Task){
+    this.onDeleteTask.emit(task)
+  }  
 }
-
